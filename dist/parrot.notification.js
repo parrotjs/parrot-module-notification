98a15d94a6be92e27012a44b959fc458ab8e09f5
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-01-25 14:21:52 +0100

    new build

--- dist/parrot.notification.js
+++ dist/parrot.notification.js
@@ -1,8 +1,8 @@
 /**
  * parrot-module-notification - Interact with HTML5 Notifications. It makes easy to reuse notifications..
- * @version v0.12.16
+ * @version v1.1.25
  * @link    https://github.com/parrotjs/parrot-module-notification
  * @author  Kiko Beats (https://github.com/Kikobeats)
  * @license MIT
  */
-(function(){!function(){var t;return t={},parrot.notification={getOrUpdate:function(n,i){var e;if(null==i&&(i=void 0),null!=i)for(e in i)t[n][e]=i[e];return t[n]},add:function(n){var i;return i=n.name,delete n.name,t[i]=n,this[i]=require("fn-partial")(this.getOrUpdate,i),this},show:function(t,n){var i,e;return 1===arguments.length&&"object"==typeof t&&(n=t,t=void 0),e=function(t){var n,i;try{return Notification.requestPermission(),i=t.title,delete t.title,new Notification(i,t)}catch(e){throw n=e,console.log(n),new Error("Notification API is not available.")}},null==t?e(n):(i=this.getOrUpdate(t,n),e(i))}}}()}).call(this);
+(function(){!function(){var t;return t={},parrot.notification={getOrUpdate:function(i,n){var e;if(null==n&&(n=void 0),null!=n)for(e in n)t[i][e]=n[e];return t[i]},add:function(i){var n;return n=i.name,delete i.name,t[n]=i,this[n]=require("fn-partial")(this.getOrUpdate,n),this},show:function(t,i){var n,e;return 1===arguments.length&&"object"==typeof t&&(i=t,t=void 0),e=function(t){var i,n;try{return Notification.requestPermission(),n=t.title,delete t.title,new Notification(n,t)}catch(e){throw i=e,new Error("Notification API is not available.")}},null==t?e(i):(n=this.getOrUpdate(t,i),e(n))}}}()}).call(this);
